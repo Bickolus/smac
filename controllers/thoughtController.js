0870c906a2ba9bb2735ba46178de94069384a0da
Bilal Raza <smbraza97@gmail.com>
2022-03-15 04:57:52 +0500

    added update and delete thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   // Get a single thought by id
   getSingleThought(req, res) {
-    User.findOne({ _id: req.params.thoughtId })
+    Thought.findOne({ _id: req.params.thoughtId })
     .populate({ path: "reactions", select: "-__v" })
     .select("-__v")
     .then((thought) =>
@@ -18,5 +18,43 @@ module.exports = {
         : res.json(thought)
     )
     .catch((err) => res.status(500).json(err));
+  },
+  // Create a thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((dbThoughtData) => {
+        User.findOneAndUpdate({ _id: req.body.userId }, { $addToSet: { thoughts: dbThoughtData._id } }, { new: true })
+          .then(dbUserData => 
+              !dbUserData
+                ? res.status(404).json({ message: "No user found with this id!" })
+                : res.json(dbUserData)
+          )
+          .catch((err) => res.status(500).json(err));
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+  // Update a thought
+  updateThought(req, res) {
+    Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $set: req.body }, { new: true })
+      .then((dbThoughtData) =>
+        !dbThoughtData
+          ? res.status(404).json(err)
+          : res.json(dbThoughtData)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((dbThoughtData) =>
+        !dbThoughtData
+          ? res.status(404).json(err)
+          : User.findOneAndUpdate({ _id: dbThoughtData.username }, { $pull: { thoughts: req.params.thoughtId } })
+            .then((dbThoughtData) => res.json(dbThoughtData))
+            .catch((err) => res.status(500).json(err))
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  addReaction(req, res) {
+    
   }
 };
