140ea41f70cdbb53ebe7e82aa8685254faa827ee
Bilal Raza <smbraza97@gmail.com>
2022-03-15 04:11:08 +0500

    added get all and get one thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,3 +1,22 @@
 const { User, Thought } = require("../models");
 
-module.exports = thoughtController;
+module.exports = {
+  // Get every thought
+  getAllThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a single thought by id
+  getSingleThought(req, res) {
+    User.findOne({ _id: req.params.thoughtId })
+    .populate({ path: "reactions", select: "-__v" })
+    .select("-__v")
+    .then((thought) =>
+      !thought
+        ? res.status(404).json({ message: "No thought exists with that ID!" })
+        : res.json(thought)
+    )
+    .catch((err) => res.status(500).json(err));
+  }
+};
